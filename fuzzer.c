1eed8c16d151d0b266776bc9031fad718f2a878c
Aarush Kumbhakern <aarushbeleren11@gmail.com>
2023-10-26 02:10:09 +0530

    INITIAL COMMIT

---
+++ fuzzer.c
@@ -0,0 +1,154 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "uthash.h"
+
+// token definitions
+#define START_TOKEN "<start>"
+#define DEPTH_LOCK_TOKEN "<dlock>"
+
+// expansion cost definitions
+#define LOW_COST 0
+#define HIGH_COST 1
+#define RAND_COST rand() % 2
+
+// depth lock states
+enum depth_lock_states {unlocked, locking, locked};
+
+// string slice function-like macro
+#define SLICE(target, source, start, end)			\
+	char target[(end - start) + 1];					\
+	memcpy(target, source + start, end - start);	\
+	target[end - start] = '\0';
+
+// hashable rule structure
+typedef struct Rule {
+	char const key[32]; // uthash hashtable key
+	size_t const expansion_count[2]; // count of expansion options
+	char const** expansions[2]; // expansion options ([0]: cheap, [1]: costly)
+	UT_hash_handle hh;
+} Rule;
+
+// declare functions
+void fuzzer(Rule const* grammar, unsigned int min_depth, unsigned int max_depth);
+char const* get_expansion(Rule const* rule, unsigned int cost);
+Rule const* get_rule(Rule const* grammar, char* key);
+
+int main(int argc, char const *argv[]) {
+	srand((unsigned) time(0)); // initialize random
+
+	Rule const* grammar = {0}; // declare grammar as pointers to rules
+
+	// define grammar rules
+	Rule start = { .key=START_TOKEN, .expansion_count={1,0}, .expansions={ 
+		(char const*[]) {"<phone>"},
+	} };
+	Rule phone = { .key="<phone>", .expansion_count={2,0}, .expansions={ 
+		(char const*[]) {"<area><number>-<number>", "<number>-<number>"},
+	} };	
+	Rule area = { .key="<area>", .expansion_count={1,0}, .expansions={ 
+		(char const*[]) {"(+<digit><digit>)"},
+	} };
+	Rule number = { .key="<number>", .expansion_count={1,1}, .expansions={ 
+		(char const*[]) {"<digit>"},
+		(char const*[]) {"<digit><number>"},
+	} };
+	Rule digit = { .key="<digit>", .expansion_count={10,0}, .expansions={ 
+		(char const*[]) {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"},
+	} };
+	
+	// add rules to grammar
+	HASH_ADD_INT(grammar, key, &start);
+	HASH_ADD_INT(grammar, key, &phone);
+	HASH_ADD_INT(grammar, key, &area);
+	HASH_ADD_INT(grammar, key, &number);
+	HASH_ADD_INT(grammar, key, &digit);
+
+	// set options from command line if possible otherwise default
+	unsigned int min_depth = argc > 1 ? strtod(argv[1], 0) : 2;
+	unsigned int max_depth = argc > 1 ? (argc > 2 ? strtod(argv[2], 0) : min_depth) : 4;
+	unsigned int runs = argc > 3 ? strtod(argv[3], 0) : 1;
+	
+	// main loop
+	for (size_t i = 0; i < runs; i++) {
+		fuzzer(grammar, min_depth, max_depth);
+	}
+
+	return EXIT_SUCCESS;
+}
+
+// fuzzing function
+void fuzzer(Rule const* grammar, unsigned int min_depth, unsigned int max_depth) {
+	// declare stack and output
+	char stack[1048576] = START_TOKEN;
+	char output[1048576] = "";
+
+	// recursion limit variables
+	unsigned int cost = 0;
+	unsigned int depth = 0;
+	unsigned int depth_lock = 0;
+
+	// while stack not empty
+	while (strlen(stack) > 0) {
+		if (stack[0] == '<') { // if first token in stack indicates nonterminal...
+			// slice token and (remaining) buffer out of stack
+			SLICE(token, stack, 0, strcspn(stack, ">") + 1);
+			SLICE(buffer, stack, strcspn(stack, ">") + 1, strlen(stack));
+
+			if (strcmp(token, DEPTH_LOCK_TOKEN) == 0) { // if current token is depth lock token...
+				depth = 0;
+				strcpy(stack, buffer); // write buffer to stack
+				depth_lock = unlocked;
+			} else {
+				Rule const* rule = get_rule(grammar, token); // get rule
+
+				// set cost based on recursion limits
+				if (depth < min_depth) cost = HIGH_COST;
+				else if (depth >= max_depth) cost = LOW_COST;
+				else cost = RAND_COST;
+
+				cost = rule->expansion_count[1] ? cost : LOW_COST; // force low cost if rule not recursive
+
+				// increment depth if high cost (recursive) expansion
+				if (cost == HIGH_COST) {
+					if (depth_lock == unlocked) depth_lock = locking;
+					depth++;
+				}
+
+				strcpy(stack, get_expansion(rule, cost)); // write random(?) expansion to stack
+
+				if (depth_lock == locking) { // if in locking stage...
+					depth_lock = locked;
+					strcat(stack, DEPTH_LOCK_TOKEN); // ...append lock token to stack
+				}
+
+				strcat(stack, buffer); // append buffer to stack
+			}
+
+		} else { // ...otherwise if first token in stack is terminal
+			// slice leading nonterminal tokens and (remaining) buffer out of stack
+			SLICE(nonterms, stack, 0, strcspn(stack, "<"));
+			SLICE(buffer, stack, strcspn(stack, "<"), strlen(stack));
+
+			// append nonterminals to output and write buffer to stack
+			strcat(output, nonterms);
+			strcpy(stack, buffer);
+		}
+	}
+
+	printf("%s\n", output); // print output
+}
+
+// get expansion from rule
+char const* get_expansion(Rule const* rule, unsigned int cost) {
+	size_t choice = rand() % rule->expansion_count[cost]; // random value between 0 and expansions count for given cost
+	return rule->expansions[cost][choice];
+}
+
+// get rule from grammar
+Rule const* get_rule(Rule const* grammar, char key[]) {
+	Rule* rule = {0};
+	HASH_FIND_INT(grammar, key, rule);
+	return rule;
+}
