0603d41d264d19a838b466a07b6800e14cca1bdf
Aarush Kumbhakern <aarushbeleren11@gmail.com>
2023-11-02 11:13:59 +0530

    GET RID OF CONST QUALIFIERS

--- fuzzer.c
+++ fuzzer.c
@@ -23,43 +23,43 @@
 
 // hashable rule structure
 typedef struct Definition {
-	char const key[32]; // uthash hashtable key
-	size_t const rule_count[2]; // count of expansion options
-	char const** rules[2]; // expansion options ([0]: cheap, [1]: costly)
+	char key[32]; // uthash hashtable key
+	size_t rule_count[2]; // count of expansion options
+	char** rules[2]; // expansion options ([0]: cheap, [1]: costly)
 	UT_hash_handle hh;
 } Definition;
 
 enum depth_lock_states {unlocked, locking, locked}; // depth lock states
 
 // declare functions
-void fuzzer(Definition const* grammar, unsigned int min_depth, unsigned int max_depth);
-char const* get_rule(Definition const* rule, unsigned int cost);
-Definition const* get_definition(Definition const* grammar, char* key);
-char* append(char* target_ptr, char const source[], size_t len);
-void slice(char target[], char const source[], size_t start, size_t end);
-char* prepend(char target[], char* target_ptr, char const source[], size_t target_len, size_t source_len);
+void fuzzer(Definition* grammar, unsigned int min_depth, unsigned int max_depth);
+char* get_rule(Definition* rule, unsigned int cost);
+Definition* get_definition(Definition* grammar, char* key);
+char* append(char* target_ptr, char source[], size_t len);
+void slice(char target[], char source[], size_t start, size_t end);
+char* prepend(char target[], char* target_ptr, char source[], size_t target_len, size_t source_len);
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char *argv[]) {
 	srand((unsigned) time(0)); // initialize random
 
-	Definition const* grammar = {0}; // declare grammar as pointers to rules
+	Definition* grammar = {0}; // declare grammar as pointers to rules
 
 	// define grammar rules
 	Definition start = { .key=START_TOKEN, .rule_count={1,0}, .rules={ 
-		(char const*[]) {"<phone>"},
+		(char*[]) {"<phone>"},
 	} };
 	Definition phone = { .key="<phone>", .rule_count={2,0}, .rules={ 
-		(char const*[]) {"<area><number>-<number>", "<number>-<number>"},
+		(char*[]) {"<area><number>-<number>", "<number>-<number>"},
 	} };	
 	Definition area = { .key="<area>", .rule_count={1,0}, .rules={ 
-		(char const*[]) {"(+<digit><digit>)"},
+		(char*[]) {"(+<digit><digit>)"},
 	} };
 	Definition number = { .key="<number>", .rule_count={1,1}, .rules={ 
-		(char const*[]) {"<digit>"},
-		(char const*[]) {"<digit><number>"},
+		(char*[]) {"<digit>"},
+		(char*[]) {"<digit><number>"},
 	} };
 	Definition digit = { .key="<digit>", .rule_count={10,0}, .rules={ 
-		(char const*[]) {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"},
+		(char*[]) {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"},
 	} };
 	
 	// add rules to grammar
@@ -83,7 +83,7 @@ int main(int argc, char const *argv[]) {
 }
 
 // fuzzing function
-void fuzzer(Definition const* grammar, unsigned int min_depth, unsigned int max_depth) {
+void fuzzer(Definition* grammar, unsigned int min_depth, unsigned int max_depth) {
 	// [TODO) dynamically allocate stack and output memory
 	// declare stack and output
 	char* stack = malloc(2097152 * sizeof(char));
@@ -119,7 +119,7 @@ void fuzzer(Definition const* grammar, unsigned int min_depth, unsigned int max_
 				depth = 0;
 
 			} else { // ...otherwise if current token is expandable
-				Definition const* definition = get_definition(grammar, tokens); // get definition
+				Definition* definition = get_definition(grammar, tokens); // get definition
 
 				// calculate cost based on depth and force low if definition not recursive
 				unsigned int cost = depth < min_depth ? HIGH_COST : (depth >= max_depth ? LOW_COST : RAND_COST);
@@ -131,7 +131,7 @@ void fuzzer(Definition const* grammar, unsigned int min_depth, unsigned int max_
 					depth++;
 				}
 
-				char const* rule = get_rule(definition, cost); // get rule
+				char* rule = get_rule(definition, cost); // get rule
 
 				if (recursion_lock_status == locking) { // if in locking stage...
 					stack_ptr = prepend(stack, stack_ptr, DEPTH_LOCK_TOKEN, STACK_LEN, strlen(DEPTH_LOCK_TOKEN)); // ...prepend lock token to stack
@@ -157,34 +157,34 @@ void fuzzer(Definition const* grammar, unsigned int min_depth, unsigned int max_
 }
 
 // get rule from definition
-char const* get_rule(Definition const* definition, unsigned int cost) {
+char* get_rule(Definition* definition, unsigned int cost) {
 	// [TODO) use a faster random number generator (SFMT?)
 	size_t choice = rand() % definition->rule_count[cost]; // random value between 0 and rule count for given cost
 	return definition->rules[cost][choice];
 }
 
 // get definition from grammar
-Definition const* get_definition(Definition const* grammar, char key[]) {
+Definition* get_definition(Definition* grammar, char key[]) {
 	Definition* definition = {0};
 	HASH_FIND_INT(grammar, key, definition);
 	return definition;
 }
 
 // get string segment
-void slice(char target[], char const source[], size_t start, size_t end) {
+void slice(char target[], char source[], size_t start, size_t end) {
 	// target = realloc(target, ((end - start) + 1) * sizeof(char));
 	memcpy(target, source + start, (end - start) * sizeof(char));
 	target[end - start] = '\0';
 }
 
 // append to traced string
-char* append(char* target_ptr, char const source[], size_t len) {
+char* append(char* target_ptr, char source[], size_t len) {
 	memmove(target_ptr, source, (len + 1) * sizeof(char));
 	return target_ptr + len;
 }
 
 // prepend to traced string
-char* prepend(char target[], char* target_ptr, char const source[], size_t target_len, size_t source_len) {
+char* prepend(char target[], char* target_ptr, char source[], size_t target_len, size_t source_len) {
 	memmove(&target[source_len], target, (target_len + 1) * sizeof(char));
 	memcpy(target, source, source_len);
 	return target + source_len + target_len;
