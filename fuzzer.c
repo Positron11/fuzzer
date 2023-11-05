85715d7c5a7a0f1132fd0cd6d3bffaa529924ec6
Positron11 <aarushbeleren11@gmail.com>
2023-11-05 20:19:21 +0530

    CLEAN UP AND INCREASE PARITY

--- fuzzer.c
+++ fuzzer.c
@@ -4,133 +4,118 @@
 #include <time.h>
 #include <limits.h>
 
-#define START_TOKEN INT_MIN
-
-// expansion cost definitions
 #define LOW_COST 0
 #define HIGH_COST 1
 #define RAND_COST rand() % 2
 
-// get definition function-like macro
-#define GET_DEFINITION(grammar, token)					\
-	(grammar->definitions)[-(START_TOKEN - token)]
+#define BUFFER_LEN (token_count - 1) // (jank, but I'll keep it for now)
 
-// rule structure
+typedef signed char token_t;
+typedef size_t depth_t;
+
 typedef struct Rule {
-	size_t const token_count;
-	int const* tokens;
+	size_t token_count;
+	token_t* tokens;
 } Rule;
 
-// definition structure
 typedef struct Definition {
-	char const* name;
-	size_t const rule_count[2];
-	Rule const* rules[2]; // [0]: cheap rules, [1]: costly rules
+	char* name;
+	size_t rule_count[2];
+	Rule* rules[2]; // [0]: cheap, [1]: costly
 } Definition;
 
-// grammar structure
 typedef struct Grammar {
-	size_t const def_count;
-	Definition const* definitions;
+	size_t def_count;
+	Definition* definitions;
 } Grammar;
 
-// function declarations
-void fuzzer(Grammar* grammar, unsigned int min_depth, unsigned int max_depth, unsigned int depth, int const* tokens, int token_count);
-Rule const* get_rule(Grammar* grammar, int token, int cost);
+enum nonterminals {start = SCHAR_MIN, phone, area, number, digit, depthlock};
 
-int main(int argc, char const *argv[]) {
+void fuzzer(Grammar* grammar, depth_t min_depth, depth_t max_depth, depth_t depth, token_t* tokens, size_t token_count);
+Rule* get_rule(Definition* definition, int cost);
+
+int main(int argc, char *argv[]) {
 	srand((unsigned) time(0)); // initialize random
 
-	// define grammar
-	enum nonterminals {
-		start = START_TOKEN, 
-		phone, 
-		area, 
-		number, 
-		digit,
-	};
 	Grammar grammar = { .def_count=5, .definitions=(Definition []) {
 		(Definition) { .name="start", .rule_count={1, 0}, .rules={
 			(Rule []) {
-				(Rule) { .token_count=1, .tokens=(int const[]) {phone} }
+				(Rule) { .token_count=1, .tokens=(token_t[]) {phone} }
 			}
 		} },
 		(Definition) { .name="phone", .rule_count={2, 0}, .rules={
 			(Rule []) {
-				(Rule) { .token_count=3, .tokens=(int const[]){number, '-', number} },
-				(Rule) { .token_count=4, .tokens=(int const[]){area, number, '-', number} }
+				(Rule) { .token_count=3, .tokens=(token_t[]){number, '-', number} },
+				(Rule) { .token_count=4, .tokens=(token_t[]){area, number, '-', number} }
 			}
 		} },
 		(Definition) { .name="area", .rule_count={1, 0}, .rules={
 			(Rule []) {
-				(Rule) { .token_count=5, .tokens=(int const[]){'(', '+', digit, digit, ')'} }
+				(Rule) { .token_count=5, .tokens=(token_t[]){'(', '+', digit, digit, ')'} }
 			}
 		} },
 		(Definition) { .name="number", .rule_count={1, 1}, .rules={
 			(Rule []) {
-				(Rule) { .token_count=1, .tokens=(int const[]){digit} },
+				(Rule) { .token_count=1, .tokens=(token_t[]){digit} },
 			},
 			(Rule []) {
-				(Rule) { .token_count=2, .tokens=(int const[]){number, digit} }
+				(Rule) { .token_count=2, .tokens=(token_t[]){number, digit} }
 			}
 		} },
 		(Definition) { .name="digit", .rule_count={10, 0}, .rules={
 			(Rule []) {
-				(Rule) { .token_count=1, .tokens=(int const[]){'0'} },
-				(Rule) { .token_count=1, .tokens=(int const[]){'1'} },
-				(Rule) { .token_count=1, .tokens=(int const[]){'2'} },
-				(Rule) { .token_count=1, .tokens=(int const[]){'3'} },
-				(Rule) { .token_count=1, .tokens=(int const[]){'4'} },
-				(Rule) { .token_count=1, .tokens=(int const[]){'5'} },
-				(Rule) { .token_count=1, .tokens=(int const[]){'6'} },
-				(Rule) { .token_count=1, .tokens=(int const[]){'7'} },
-				(Rule) { .token_count=1, .tokens=(int const[]){'8'} },
-				(Rule) { .token_count=1, .tokens=(int const[]){'9'} },
+				(Rule) { .token_count=1, .tokens=(token_t[]){'0'} },
+				(Rule) { .token_count=1, .tokens=(token_t[]){'1'} },
+				(Rule) { .token_count=1, .tokens=(token_t[]){'2'} },
+				(Rule) { .token_count=1, .tokens=(token_t[]){'3'} },
+				(Rule) { .token_count=1, .tokens=(token_t[]){'4'} },
+				(Rule) { .token_count=1, .tokens=(token_t[]){'5'} },
+				(Rule) { .token_count=1, .tokens=(token_t[]){'6'} },
+				(Rule) { .token_count=1, .tokens=(token_t[]){'7'} },
+				(Rule) { .token_count=1, .tokens=(token_t[]){'8'} },
+				(Rule) { .token_count=1, .tokens=(token_t[]){'9'} },
 			}
 		} }
 	} };
 
 	// set options from command line if possible otherwise default
-	unsigned int min_depth = argc > 1 ? strtod(argv[1], 0) : 2;
-	unsigned int max_depth = argc > 1 ? (argc > 2 ? strtod(argv[2], 0) : min_depth) : 4;
-	unsigned int runs = argc > 3 ? strtod(argv[3], 0) : 1;
+	depth_t min_depth = argc > 1 ? strtod(argv[1], 0) : 2;
+	depth_t max_depth = argc > 1 ? (argc > 2 ? strtod(argv[2], 0) : min_depth) : 4;
+	depth_t runs = argc > 3 ? strtod(argv[3], 0) : 1;
 	
-	// main loop
-	for (size_t i = 0; i < runs; i++) {
-		fuzzer(&grammar, min_depth, max_depth, 0, (int const[]) {START_TOKEN}, 1);
-	}
+	for (size_t i = 0; i < runs; i++) fuzzer(&grammar, min_depth, max_depth, 0, (token_t[]) {start}, 1);
 
 	return EXIT_SUCCESS;
 }
 
-// fuzzer function
-void fuzzer(Grammar* grammar, unsigned int min_depth, unsigned int max_depth, unsigned int depth, int const* tokens, int token_count) {
-	int token = tokens[0]; // get token
+// main fuzzer function
+void fuzzer(Grammar* grammar, depth_t min_depth, depth_t max_depth, depth_t current_depth, token_t* tokens, size_t token_count) {
+	token_t token = tokens[0]; // get first token
 
-	if (token >= 0) { // if token is terminal...
-		putchar(token); // print to stdout
+	// if token is terminal print to stdout
+	if (token >= 0) {
+		putchar(token);
 
-	} else { // ...otherwise if token is nonterminal
-		// calculate cost based on depth and force low if def not recursive
-		unsigned int cost = depth < min_depth ? HIGH_COST : (depth >= max_depth ? LOW_COST : RAND_COST);		
-		cost = GET_DEFINITION(grammar, token).rule_count[1] ? cost : LOW_COST;
+	} else {
+		Definition* definition = &grammar->definitions[token - start];
+
+		int rule_cost = current_depth < min_depth ? HIGH_COST : (current_depth >= max_depth ? LOW_COST : RAND_COST); // calculate cost based on depth...
+		rule_cost = definition->rule_count[1] ? rule_cost : LOW_COST; // ...and force low if definition not recursive
 		
-		// get rule and fuzz
-		Rule const* rule = get_rule(grammar, token, cost);
-		fuzzer(grammar, min_depth, max_depth, depth + 1, rule->tokens, rule->token_count);
+		Rule* rule = get_rule(definition, rule_cost);
+
+		fuzzer(grammar, min_depth, max_depth, current_depth + 1, rule->tokens, rule->token_count); // fuzz the rule's expansion
 	}
 	
-	// if buffer exists, get buffer and fuzz
-	int buffer_len = token_count - 1;
-	if (buffer_len) {
-		int buffer[buffer_len];
-		memcpy(buffer, tokens + 1, buffer_len*sizeof(int));
-		fuzzer(grammar, min_depth, max_depth, depth, buffer, buffer_len);
+	if (BUFFER_LEN) { // if buffer exists...
+		token_t buffer[BUFFER_LEN];
+		memcpy(buffer, tokens + 1, BUFFER_LEN*sizeof(token_t));
+
+		fuzzer(grammar, min_depth, max_depth, current_depth, buffer, BUFFER_LEN); // ...fuzz the buffer
 	}
 }
 
-// get rule by key
-Rule const* get_rule(Grammar* grammar, int token, int cost) {
-	Definition definition = GET_DEFINITION(grammar, token);
-	return &((definition.rules)[cost])[rand() % (definition.rule_count)[cost]];
+// get rule from definition
+Rule* get_rule(Definition* definition, int cost) {
+	return &((definition->rules)[cost])[rand() % (definition->rule_count)[cost]];
 }
