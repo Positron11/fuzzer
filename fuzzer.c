80e3d78fc8fd0690d0ab419f76b472323d3e4b69
Positron11 <aarushbeleren11@gmail.com>
2023-11-05 19:57:30 +0530

    CLEAN UP CODE

--- fuzzer.c
+++ fuzzer.c
@@ -4,47 +4,39 @@
 #include <time.h>
 #include <limits.h>
 
-// cost definitions
 #define LOW_COST 0
 #define HIGH_COST 1
 #define RAND_COST rand() % 2
 
-// computed definitions
-#define STACK_LEN stack_ptr - stack
+#define STACK_LEN stack_ptr - stack // (jank, but I'll keep it for now)
 
-// overrwrite function-like macro
+// append function augmenter
 #define OVERWRITE(target, target_ptr, source, len)		\
 	target_ptr = target;								\
 	target_ptr = append(target_ptr, source, len);
 
-// type definitions
 typedef signed char token_t;
 typedef size_t depth_t;
 
-// rule structure
 typedef struct Rule {
 	size_t token_count;
 	token_t* tokens;
 } Rule;
 
-// definition structure
 typedef struct Definition {
 	char* name;
 	size_t rule_count[2];
 	Rule* rules[2]; // [0]: cheap, [1]: costly
 } Definition;
 
-// grammar structure
 typedef struct Grammar {
 	size_t def_count;
 	Definition* definitions;
 } Grammar;
 
-// enumerated constants
 enum depth_lock_states {unlocked, locking, locked};
 enum nonterminals {start = SCHAR_MIN, phone, area, number, digit, depthlock};
 
-// function declarations
 void fuzzer(Grammar* grammar, depth_t min_depth, depth_t max_depth);
 Rule* get_rule(Definition* definition, int cost);
 token_t* append(token_t* target_ptr, token_t source[], size_t len);
@@ -53,7 +45,6 @@ token_t* prepend(token_t target[], token_t* target_ptr, token_t source[], size_t
 int main(int argc, char *argv[]) {
 	srand((unsigned) time(0)); // initialize random
 
-	// define grammar
 	Grammar grammar = { .def_count=5, .definitions=(Definition []) {
 		[start - start] = (Definition) { .name="start", .rule_count={1, 0}, .rules={
 			(Rule []) {
@@ -100,17 +91,13 @@ int main(int argc, char *argv[]) {
 	depth_t max_depth = argc > 1 ? (argc > 2 ? strtod(argv[2], 0) : min_depth) : 4;
 	depth_t runs = argc > 3 ? strtod(argv[3], 0) : 1;
 	
-	// main loop
-	for (size_t i = 0; i < runs; i++) {
-		fuzzer(&grammar, min_depth, max_depth);
-	}
+	for (size_t i = 0; i < runs; i++) fuzzer(&grammar, min_depth, max_depth);
 
 	return EXIT_SUCCESS;
 }
 
-// fuzzer function
+// main fuzzer function
 void fuzzer(Grammar* grammar, depth_t min_depth, depth_t max_depth) {
-	// declare stack and output
 	token_t* stack = malloc(2097152 * sizeof(token_t));
 	token_t* output = malloc(2097152 * sizeof(token_t));
 
@@ -118,26 +105,28 @@ void fuzzer(Grammar* grammar, depth_t min_depth, depth_t max_depth) {
 	token_t* stack_ptr = stack;
 	token_t* out_ptr = output;
 
-	*(stack_ptr++) = start; // initialize stack
+	*(stack_ptr++) = start; // initialize stack with start token
 
 	// recursion limit variables
-	int rule_cost = 0;
-	depth_t current_depth = 0;
 	int recursion_lock_state = 0;
+	depth_t current_depth = 0;
+	int rule_cost = 0;
 
-	// while stack not emmpty
+	// while stack not empty...
 	while (STACK_LEN > 0) {
-		token_t token = stack[0]; // get token
+		token_t token = stack[0]; // get first token
 		
 		int buffer_len = STACK_LEN - 1; // get buffer length
 		OVERWRITE(stack, stack_ptr, &stack[1], buffer_len); // overrwrite stack with buffer
 
-		if (token >= 0) { // if token is terminal
-			*(out_ptr++) = token; // append token to output6
+		// if token is terminal append token to output
+		if (token >= 0) {
+			*(out_ptr++) = token;
 			continue;
 		}
 		
-		if (token == depthlock) { // if token is depthlock token
+		// if segment is depthlock token reset recursion lock vars
+		if (token == depthlock) {
 			recursion_lock_state = unlocked;
 			current_depth = 0;
 			continue;	
@@ -145,11 +134,11 @@ void fuzzer(Grammar* grammar, depth_t min_depth, depth_t max_depth) {
 		
 		Definition* definition = &grammar->definitions[token - start];
 		
-		rule_cost = current_depth < min_depth ? HIGH_COST : (current_depth >= max_depth ? LOW_COST : RAND_COST); // calculate cost based on depth
-		rule_cost = definition->rule_count[1] ? rule_cost : LOW_COST; // force low if definition not recursive
+		rule_cost = current_depth < min_depth ? HIGH_COST : (current_depth >= max_depth ? LOW_COST : RAND_COST); // calculate cost based on depth...
+		rule_cost = definition->rule_count[1] ? rule_cost : LOW_COST; // ...and force low if definition not recursive
 		
 		if (rule_cost == HIGH_COST) {
-			if (recursion_lock_state == unlocked) recursion_lock_state = locking; // if in locking state promote to locked
+			if (recursion_lock_state == unlocked) recursion_lock_state = locking;
 			current_depth++;
 		}
 
@@ -163,7 +152,7 @@ void fuzzer(Grammar* grammar, depth_t min_depth, depth_t max_depth) {
 		stack_ptr = prepend(stack, stack_ptr, rule->tokens, STACK_LEN, rule->token_count); // prepend rule to stack
 	}
 
-	for (size_t i = 0; i < out_ptr - output; i++) putchar(output[i]); // print output
+	for (size_t i = 0; i < out_ptr - output; i++) putchar(output[i]);
 }
 
 // get rule from definition
