2d9d3c87e56fb85e6a0981d859a4308cdce24d44
Positron11 <aarushbeleren11@gmail.com>
2023-11-08 22:52:01 +0530

    LET MAIN HANDLE COMPILING AND EXECUTING

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import json
-import types
+import subprocess
 from compiler import gen_fuzz_src as compile
+
+filename = "fuzzer"
+max_depth = 100
 
 with open("grammar.json", "r") as f:
 	grammar = json.load(f)
 
-source = compile(grammar)
-print(source)
+with open(f"{filename}.c", "w") as f:
+	source = compile(grammar)
+	print(source, file=f)
+
+subprocess.run(f"gcc -o {filename}.o {filename}.c", shell=True)
+subprocess.run(f"./{filename}.o {max_depth}", shell=True)
+
+
 
