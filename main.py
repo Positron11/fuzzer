becd54f4569e8cb4a9b9b2cc2cb78a9cba47493d
Positron11 <aarushbeleren11@gmail.com>
2023-11-10 23:32:34 +0530

    GENERATE EXECUTABLE

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import os
 import json
-# import subprocess
+import subprocess
 from compiler import gen_header_src, gen_main_src
 
 os.makedirs("./build", exist_ok=True) 
 
 filename = "fuzzing"
-max_depth = 100
+depth = 100
 
 with open("grammar.json", "r") as f:
 	grammar = json.load(f)
@@ -19,15 +19,15 @@ with open(f"build/{filename}.c", "w") as f:
 	source = gen_main_src(grammar, filename)
 	print(source, file=f)
 
-# # run this segment to create, compile, and run test driver c code
-# with open("build/main.c", "w") as f:	
-# 	print( "#include <stdlib.h>\n"											\
-# 		  f"#include \"{filename}.h\"\n\n"									\
-# 			"int main(int argc, char const *argv[]) {\n"					\
-# 			"\tint max_depth = argc > 1 ? strtod(argv[1], 0) : 10;\n"		\
-# 			"\tfuzz(max_depth);\n"											\
-# 			"\treturn 0;\n"													\
-# 			"}\n", file=f)
+# generate and compile executable driver
+with open("build/main.c", "w") as f:	
+	print( "#include <stdlib.h>\n"												\
+		  f"#include \"{filename}.h\"\n\n"										\
+			"int main(int argc, char const *argv[]) {\n"						\
+			"\tint min_depth = argc > 1 ? strtod(argv[1], 0) : 0;\n"			\
+			"\tint max_depth = argc > 1 ? strtod(argv[argc - 1], 0) : 10;\n"		\
+			"\tfuzz(min_depth, max_depth);\n"									\
+			"\treturn 0;\n"														\
+			"}\n", file=f)
 
-# subprocess.run(f"gcc -o build/fuzzer build/main.c build/{filename}.c", shell=True)									
-# subprocess.run(f"./build/fuzzer {max_depth}", shell=True)									
+subprocess.run(f"gcc -o build/fuzzer build/main.c build/{filename}.c", shell=True)							
